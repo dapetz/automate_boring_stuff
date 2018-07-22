b161e93afa5f71e3200c18434d3aeb490fc71756
dapetz <41242907+dapetz@users.noreply.github.com>
2018-07-21 23:10:33 -0400

    Create tablePrinter.py

---
+++ chapter4/tablePrinter.py
@@ -0,0 +1,21 @@
+#Automate the Boring Stuff with Python - Chapter 4
+#Table Print practice program
+
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+def printTable(data):
+    colWidths = [0] * len(data)     #create list to store len values
+    for i in colWidths:
+        colWidths = len(max(data[i], key=len))
+        
+    
+    for r in range(len(data[0])):
+        print()
+        for c in range(len(data)):
+            print(data[c][r].rjust(colWidths), end='')
+            
+    return data
+
+printTable(tableData)
